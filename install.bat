57b74b48a06a8cf850001ef0eb13e870d554c0e8
Sava Sebastian-Florin <67977875+SebastianSava@users.noreply.github.com>
2022-07-19 20:34:29 +0300

    Updates

--- install.bat
+++ install.bat
@@ -1,26 +1,26 @@
 @echo off
 
 :: install extensions
-cd "C:\Users\Asus\Desktop"
-wget -O "extensions.ps1" "https://sava-sebastian.dev/VSCode/extensions.txt"
+cd "C:\Users\ASUS\Desktop"
+wget -O "extensions.ps1" "https://sebastiansava.github.io/VSCode_Windows/extensions.txt"
 Powershell.exe - < extensions.ps1
 del extensions.ps1
 
 :: go to VSCode installation folder
-cd "C:\Users\Asus\AppData\Roaming\Code\User"
+cd "C:\Users\ASUS\AppData\Roaming\Code\User"
 
 :: download 'settings' file
-wget -O "settings.json" "https://sava-sebastian.dev/VSCode/settings.json"
+wget -O "settings.json" "https://sebastiansava.github.io/VSCode_Windows/settings.json"
 
 :: download 'keybindings' file
-wget -O "keybindings.json" "https://sava-sebastian.dev/VSCode/keyboard.json"
+wget -O "keybindings.json" "https://sebastiansava.github.io/VSCode_Windows/keyboard.json"
 
 :: go to 'snippets' folder
 if not exist "snippets" mkdir "snippets"
 cd "./snippets"
 
 :: download 'snippets' file
-wget -O "global.code-snippets" "https://sava-sebastian.dev/VSCode/snippets.json"
+wget -O "global.code-snippets" "https://sebastiansava.github.io/VSCode_Windows/snippets.json"
 
 :: exit
 exit
